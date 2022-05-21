aa98f6c204a12e02cfcd751b953052d9f60466ea
vikashsingh009 <vikash.singh@bizruntime.com>
2022-05-21 11:58:49 +0530

    first commit

---
+++ earrings/webpack.config.js
@@ -0,0 +1,23 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const path = require('path');
+
+
+
+module.exports = {
+    entry: './earrings3D/main.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: './earrings3D/index.html'}),
+        new webpack.ProvidePlugin({
+            THREE: 'three'
+        }),
+        new CopyWebpackPlugin([
+            './earrings3D/assets/**'
+        ])
+    ]
+};
